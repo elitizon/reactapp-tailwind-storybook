7773f6a26f9d16e1ade270459ec4e535629c4521
Raphael MANSUY <raphael.mansuy@caliatys.com>
2021-01-16 21:14:50 +0800

    fix linting

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,6 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
